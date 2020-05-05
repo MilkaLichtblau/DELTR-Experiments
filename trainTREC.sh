36c0e2922b992543a8a9ae83807248b9954a6107
Meike Zehlike <meike.zehlike@tu-berlin.de>
2020-05-05 17:09:17 +0200

    script

--- trainTREC.sh
+++ trainTREC.sh
@@ -3,7 +3,6 @@
 
 # exit when any command fails
 set -e
-
 # keep track of the last executed command
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
